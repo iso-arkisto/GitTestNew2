a026988b5fbe9734c033c93ce1b536eb0ee441f9
iso-arkisto <Fian.123@yandex.ru>
2025-12-12 18:02:24 +0300

    Revert "add HelloScreen.kt with label"

--- app/src/main/java/com/yourname/gittest/MainActivity.kt
+++ app/src/main/java/com/yourname/gittest/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.yourname.gittest.screens.HelloScreen
 import com.yourname.gittest.ui.theme.GitTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HelloScreen()
+
                 }
             }
         }
